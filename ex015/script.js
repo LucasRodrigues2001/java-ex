659da53bf94c484aaa836a0b5aa1e029888b5116
LucasRodrigues2001 <lucas5522santos@gmail.com>
2024-11-05 19:22:42 -0300

    Add files via upload

---
+++ ex015/script.js
@@ -0,0 +1,4 @@
+var valor = Number(prompt("coloque o valor da largura do retangulo "))
+var valorDois = Number(prompt("coloque o valor da altura do retangulo "))
+var resultado = valor * valorDois
+alert(`o resultado é ${resultado}`)
